41052948cf3ee5520aa08fdddaaa15a0bb30335a
Devid Koch <83297720+devid-koch@users.noreply.github.com>
2024-06-20 11:04:23 +0530

    Update App.jsx

--- text_client/src/App.jsx
+++ text_client/src/App.jsx
@@ -1,24 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import socketIOClient from 'socket.io-client';
-import { TailSpin } from 'react-loader-spinner'; // Import the loading spinner component
 
-const ENDPOINT = "http://localhost:4000";
+const ENDPOINT = "https://assigment-api.onrender.com";
 
 function App() {
     const [messages, setMessages] = useState([]);
-    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         const socket = socketIOClient(ENDPOINT, {
-            transports: ['websocket', 'polling'] // Ensure socket.io uses websocket and polling transports
+            transports: ['websocket', 'polling']
         });
 
         socket.on("message", (data) => {
             setMessages(data.text ? data.text : "Want to regenerate again please reload");
-            setLoading(true);
             setTimeout(() => {
                 setLoading(false);
-            }, 1000); // Show loading spinner for 1 second
+            }, 1000);
         });
 
         socket.on('connect_error', (err) => {
